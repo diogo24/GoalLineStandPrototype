c37ec6cad2e170a8a1a66f73f716ab197687ad83
Diogo24 <diocar24@hotmail.com>
2016-03-09 13:04:18 +0000

    Goal Line Stand - Home

---
+++ GoalLineStandPrototype/GoalLineStandPrototype/Controllers/GLSHomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GoalLineStandPrototype.Controllers
+{
+    public class GLSHomeController : Controller
+    {
+        // GET: GLSHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
