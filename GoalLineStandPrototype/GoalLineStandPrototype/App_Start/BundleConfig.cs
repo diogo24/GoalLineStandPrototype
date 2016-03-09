de65b8567b8614cb4edb23805809d7988d62e742
Diogo24 <diocar24@hotmail.com>
2016-03-09 13:04:18 +0000

    Goal Line Stand Prototype - Home

--- GoalLineStandPrototype/GoalLineStandPrototype/App_Start/BundleConfig.cs
+++ GoalLineStandPrototype/GoalLineStandPrototype/App_Start/BundleConfig.cs
@@ -26,6 +26,9 @@ namespace GoalLineStandPrototype
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/GLS/css").Include(
+                                  "~/Content/goalLineStand.css"));
         }
     }
 }
